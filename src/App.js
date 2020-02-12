2151e3d9b440a57a71a7950a4563d45a93e1e21a
isPlaying <369370@gmail.com>
2020-02-12 11:32:56 +0800

    feat(*): redux todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from 'redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const init = {
+  inputValue: '',
+  list: []
+};
+const reducer = (state = init, action) => {
+  switch (action.type) {
+    case 'change':
+      return { ...state, inputValue: action.payload };
+    case 'add':
+      return {
+        ...state,
+        inputValue: '',
+        list: [...state.list, action.payload]
+      };
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleStore = this.handleStore.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = store.getState();
+    this.add = this.add.bind(this);
+    // store.subscribe(this.handleStore);
+    store.subscribe(() => this.setState(store.getState()));
+  }
+  handleStore() {
+    this.setState(store.getState());
+  }
+  handleChange(e) {
+    store.dispatch({
+      type: 'change',
+      payload: e.target.value
+    });
+  }
+  add() {
+    store.dispatch({
+      type: 'add',
+      payload: this.state.inputValue
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.add}>add</button>
+        <ul>
+          {this.state.list.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
